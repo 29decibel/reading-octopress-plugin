7753bcfe91af54d6d7a790441c5c74b78854017c
dongbin.li <mike.d.1984@gmail.com>
2012-01-27 20:11:20 +0800

    add cache support

--- plugins/reading_tag.rb
+++ plugins/reading_tag.rb
@@ -28,23 +28,56 @@ module Jekyll
   class ReadingTag < Liquid::Tag
 		@read
 		@book_url = ''
+		CACHE_INFO_SEP = '__@@__'
+
     def initialize(tag_name, markup, tokens)
 			@book_url,@read = markup.split(' ')
+			@cache_folder = File.expand_path '../caches',File.dirname(__FILE__)
+			FileUtils.mkdir_p @cache_folder unless File.exist?(@cache_folder)
+			@cache_infos = {}
+			restore_cache if cache_exist?
       super
     end
 
+		def cache_file
+			@cache_file ||= File.join(@cache_folder,'reading_tag.cache')
+		end
+
+		def cache_exist?
+			File.exist? cache_file
+		end
+
+		def restore_cache
+			File.open(cache_file) do |file|
+				while(line=file.gets)
+					url,title,img,rate = line.split(CACHE_INFO_SEP)
+					@cache_infos[url] = {:title => title,:img	=> img,:rate => rate}
+				end
+			end
+		end
+
+
     def render(context)
       output = super
-			puts "begin fetch book info of #{@book_url}"
-			# get book infos 
-			doc = Nokogiri::HTML(open(@book_url))
-			@title = doc.at_css('#btAsinTitle').content
-			if @title.length > 40
-				@title = @title[0..40] + '...'
+			# check cache
+			if @cache_infos[@book_url]
+				@title = @cache_infos[@book_url]['title']
+				@img = @cache_infos[@book_url]['img']
+				@rate = @cache_infos[@book_url]['rate']
+			else
+				puts "begin fetch book info of #{@book_url}"
+				# get book infos 
+				doc = Nokogiri::HTML(open(@book_url))
+				@title = doc.at_css('#btAsinTitle').content
+				if @title.length > 40
+					@title = @title[0..40] + '...'
+				end
+				@img = doc.at_css('img#prodImage').attr('src')
+				@rate = doc.at_css('.swSprite.s_star_4_5').attr('title')
+				# cache infos
+				save_to_cache_file(@book_url,@title,@img,@rate)
 			end
-			@img = doc.at_css('img#prodImage').attr('src')
-			@rate = doc.at_css('.swSprite.s_star_4_5').attr('title')
-
+			#render
       if @book_url
 				reading_markup = "<div class='book_info #{@read}'>"
 				reading_markup += "<a href='#{@book_url}'><img src='#{@img}'></img></a>"
@@ -55,6 +88,14 @@ module Jekyll
         "Error processing input, expected syntax: {% reading url/to/books_url_at_amazon [read reading] %}"
       end
     end
+
+		def save_to_cache_file(url,title,img,rate)
+			puts 'save to cache file'
+			File.open(cache_file,'a') do |file|
+				file.puts([url,title,img,rate].join(CACHE_INFO_SEP))
+			end
+		end
+
   end
 end
 
